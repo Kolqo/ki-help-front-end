71a110b87aa7002610a1af7ab5fcfc638bdff837
mac <mac@MacBook-Pro-mac.local>
2025-06-22 19:01:29 +0300

    commit 1.8.2 fixed useBuyingTask.js (delete args limit)

--- src/pages/task/buying-task/model/useBuyingTask.js
+++ src/pages/task/buying-task/model/useBuyingTask.js
@@ -23,13 +23,6 @@ const useBuyingTask = (fields) => {
   const handleValidation = async (subjectID, buying, task) => {
     let isError = false;
 
-    for (let i = 0; i < values.length; i++) {
-      if (values[i].length > 50 || values[i].length < 1) {
-        isError = true;
-        break;
-      }
-    }
-
     if (isError) {
       setError(true);
       setErrorMessage("Введіть коректний аргумент. Аргумент не може бути пустим")
