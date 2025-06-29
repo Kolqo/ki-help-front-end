a6bcd99f260aaf7c584e26048a8df8f3299ef4eb
mac <mac@MacBook-Pro-mac.local>
2025-06-29 13:44:30 +0300

    commit 1.8.7 update domain

--- src/pages/task/list-task/const/filterItems.js
+++ src/pages/task/list-task/const/filterItems.js
@@ -1,5 +1,5 @@
 const filterItems = (subjectID) => [
-  { id: 1, name: "Викладачч", key: "teacher", to: `/list-task/${subjectID}/choose-teacher` },
+  { id: 1, name: "Викладач", key: "teacher", to: `/list-task/${subjectID}/choose-teacher` },
   { id: 2, name: "Створено", key: "creator", to: `/list-task/${subjectID}/choose-creator` },
   { id: 3, name: "Ціна", key: "price", to: `/list-task/${subjectID}/choose-price` },
 ];
